72b5520bad80ae1c3ae99db7d47c98d81ce9a7f5
Seoa Kim <seoa521@gmail.com>
2023-07-21 17:47:12 +0900

    [feat] 영화 포스터 노출 기능 추가

---
+++ src/components/Row.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
+import "./Row.css";
+
+export default function Row({ isLargeRow, title, id, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchUrl]);
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    setMovies(request.data.results);
+    return request;
+  };
+
+  return (
+    <section className="row">
+      {/* TITLE */}
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider_arrow-left">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
+        </div>
+        <div id={id} className="row_posters">
+          {/**SEVERAL ROW_POSTER */}
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+              src={`https://image.tmdb.org/t/p/original/${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              loading="lazy"
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div className="slider_arrow-right">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            {">"}
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+}
